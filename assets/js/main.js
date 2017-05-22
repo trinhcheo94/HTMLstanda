76a9d349f2a8f6035c684bd7d2b8c0c3b6c7c79d
trinh cheo <trinhcheo54b@gmail.com>
2017-05-22 09:46:41 +0700

    Html stand

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+(function($) {
+  'use strict';
+
+  $(function() {
+
+  	var simulateTyping = "Hello World!";
+
+$('.keyboard')
+    .keyboard({
+		layout: 'custom',
+		customLayout: {
+			'normal': [
+				'` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+				'{tab} q w e r t y u i o p [ ] \\',
+				'a s d f g h j k l ; \' {enter}',
+				'{shift} z x c v b n m , . / {shift}',
+				'{accept} {space} {left} {right}'
+			],
+			'shift': [
+				'~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+				'{tab} Q W E R T Y U I O P { } |',
+				'A S D F G H J K L : " {enter}',
+				'{shift} Z X C V B N M < > ? {shift}',
+				'{accept} {space} {left} {right}'
+			]
+		}
+    })
+    .addTyping()
+    .addCaret({
+        caretClass: '',
+        // *** for future use ***
+        // data-attribute containing the character(s) next to the caret
+        charAttr: 'data-character',
+        // # character(s) next to the caret (can be negative for RTL)
+        // default is 1 which shows the character to the right of the caret
+        // setting this to -1 shows the character to the left
+        charIndex: -1,
+        // tweak caret position & height
+        offsetX: 0,
+        offsetY: 0,
+        adjustHt: 0
+    });
+
+// Typing Extension
+$('.icon').click(function () {
+    var kb = $(this).prev().getkeyboard();
+    // typeIn( text, delay, callback );
+    kb.reveal().typeIn(simulateTyping, 500, function () {
+        // do something after text is added
+        // kb.accept();
+    });
+});
+
+
+  }); // end document ready
+
+
+  $(window).resize(function() {
+
+    // set min-height panel
+    var h_window = $(window).height();
+    $("#rs-wraper").css("min-height", h_window);
+
+  });
+
+})(jQuery); // end JQuery namespace
